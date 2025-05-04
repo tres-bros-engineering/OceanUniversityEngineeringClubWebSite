c985ae45d7b34f554325ef311d3597c085b947ca
Dinidu Atapattu <90089284+atapattudinidu@users.noreply.github.com>
2025-05-05 01:04:29 +0530

    Initial project upload
    Initial project upload of the project start

---
+++ engineering-club-site-backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+// const router = require('./router');
+
+require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+const uri = process.env.URI;
+
+const connect = async () => {
+    try{
+        await mongoose.connect(uri);
+        console.log('Database connected successfully');
+    }
+    catch(error){
+        console.log('Database error: ',error)
+    }
+}
+
+connect();
+
+const server = app.listen(port,host,() => {
+    console.log(`Node server is listening to ${server.address().port}`)
+});
+
+// app.use('/api', router);
